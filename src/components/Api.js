8af186148332ca2f7fdcb62f843a34c2d51972b8
SerhyRymar <serhyrymar@gmail.com>
2023-02-12 20:07:31 +0200

    update_api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,19 +1,41 @@
 import axios from 'axios';
 
-// const API_KEY = '32160285-f1ddcc5bb333b538ce7462252';
+// З відео Репети
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export const getPictures = async (query, page) => {
-    const response = await axios.post('/materials', query, page);
-    return response.data
-}
-// export const getPictures = async (query, page) => {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const PICTURES =
-//     'q=${query}&page = ${page}&key=32160285-f1ddcc5bb333b538ce7462252&image_type=photo&orientation=horizontal&per_page=12';
-//   const response = await axios.get(`${BASE_URL}/?${PICTURES}`);
-//   return response.data;
-// };
+  const API_KEY = '32160285-f1ddcc5bb333b538ce7462252';
+  const response = await axios.get(
+    `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response.data;
+};
+
+
+// Приклад з домашки по JS 
+
+// export async function fetchPictures(name, page) {
+//   const params = new URLSearchParams({
+//     key: '32160285-f1ddcc5bb333b538ce7462252',
+//     q: name,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: 40,
+//     page,
+//   });
+
+//   try {
+//     const response = await axios({
+//       method: 'get',
+//       url: `https://pixabay.com/api/?${params}`,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.log(`Error: ${error}`);
+//   }
+// }
+
 
 
